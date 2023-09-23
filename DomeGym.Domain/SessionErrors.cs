5986dedd775ff6c79d076e7f2b08c11f4feda4d5
haoquantang <hqtang33@gmail.com>
2023-09-23 19:08:41 +0800

    Modify SessionsErrors to readonly

--- DomeGym.Domain/SessionErrors.cs
+++ DomeGym.Domain/SessionErrors.cs
@@ -4,17 +4,17 @@ namespace DomeGym.Domain;
 
 public static class SessionErrors
 {
-    public static Error CannotCancelReservationTooCloseToSession = Error.Validation(
+    public static readonly Error CannotCancelReservationTooCloseToSession = Error.Validation(
         code: "Session.TooCloseToSession",
         description: "Unable to cancel reservations."
     );
     
-    public static Error ReservationNotFound = Error.NotFound(
+    public static readonly Error ReservationNotFound = Error.NotFound(
         code: "Session.ReservationNotFound",
         description: "Reservation not found."
     );
     
-    public static Error ParticipantsLimitReached = Error.Validation(
+    public static readonly Error ParticipantsLimitReached = Error.Validation(
         code: "Session.ParticipantsLimitReached",
         description: "Cannot have more reservations than max participants."
     );
